37b5ac98d34da916f6dfa40209b033d8c6c840b7
Nella <nella611@yandex.ru>
2023-08-18 12:28:54 +0300

    change query to dish count in menu

--- models/menu.py
+++ models/menu.py
@@ -1,11 +1,11 @@
 import uuid
 
-from sqlalchemy import UUID, Column, String, func, select
+from sqlalchemy import UUID, Column, Integer, String, cast, func, select
 from sqlalchemy.orm import column_property, relationship
 
 from database import BaseDBModel
 
-from .dish import Dish
+# from .dish import Dish
 from .submenu import SubMenu
 
 
@@ -24,8 +24,14 @@ class Menu(BaseDBModel):
             SubMenu.menu_id == id).correlate_except(SubMenu).scalar_subquery()
     )
 
+    # dishes_count = column_property(
+    #     select(func.count(Dish.id)).where(Dish.submenu_id.in_(
+    #         select(SubMenu.id).where(SubMenu.menu_id == id)
+    #     )).correlate_except(Dish).scalar_subquery()
+    # )
     dishes_count = column_property(
-        select(func.count(Dish.id)).where(Dish.submenu_id.in_(
-            select(SubMenu.id).where(SubMenu.menu_id == id)
-        )).correlate_except(Dish).scalar_subquery()
+        select(func.coalesce(
+            cast(func.sum(SubMenu.dishes_count), Integer), 0
+        )).where(
+            SubMenu.menu_id == id).scalar_subquery()
     )
